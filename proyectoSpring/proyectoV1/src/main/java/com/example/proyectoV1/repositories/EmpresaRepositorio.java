6552857beb8d1750914778676229562eb431d979
juan <juan.ramirez.sk@gmail.com>
2019-07-10 16:53:31 -0400

    cambio empresa repositorio de class a interface

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/EmpresaRepositorio.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/EmpresaRepositorio.java
@@ -3,11 +3,12 @@ package com.example.proyectoV1.repositories;
 import java.util.List;
 
 import org.springframework.data.repository.Repository;
+
 import org.springframework.http.ResponseEntity;
 
 import com.example.proyectoV1.entities.ReclamoSugerencia;
 
-public class EmpresaRepositorio extends Repository<ReclamoSugerencia,Integer>{
+public interface EmpresaRepositorio extends Repository<ReclamoSugerencia,Integer>{
 	List<ReclamoSugerencia>findAll();
 	List<ReclamoSugerencia> listar();
 	ReclamoSugerencia findOne(int id);
