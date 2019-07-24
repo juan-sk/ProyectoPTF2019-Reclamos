62374a3ec6b6a8daa0a73629029d678ba2a34cf0
Vanne <vanne@Vannes-MacBook-Pro.local>
2019-07-24 14:58:26 -0400

    Registro empresas - validaciones lista y funcionando con la DB

--- FrontAngular/v1/src/app/componentes/registro-empresa/registro-empresa.component.ts
+++ FrontAngular/v1/src/app/componentes/registro-empresa/registro-empresa.component.ts
@@ -4,39 +4,96 @@ import { Trabajador } from 'src/app/Modelo/trabajador';
 import { EmpresaServiceService } from 'src/app/Services/empresa-service.service';
 import { Router } from '@angular/router';
 import { TrabajadorServiceService } from 'src/app/Services/trabajador-service.service';
+import { FormBuilder, FormGroup, Validators } from  '@angular/forms';
+import { ValidarRut } from 'src/app/funcionesDeValidacion/validarRUT';
+
 
 @Component({
   selector: 'app-registro-empresa',
   templateUrl: './registro-empresa.component.html',
-  styleUrls: ['./registro-empresa.component.css']
-})
-export class RegistroEmpresaComponent implements OnInit {
+  styleUrls: ['./registro-empresa.component.css']})
 
-  constructor(private router:Router, private empresaService:EmpresaServiceService,private trabajadorServicio:TrabajadorServiceService) { }
+
+export class RegistroEmpresaComponent implements OnInit {
   empresa:Empresa=new Empresa();
   trabajador:Trabajador=new Trabajador();
-  pass2:string;
+  rutEmpresa:string;
+  nombreEmpresa:string;
+  emailEmpresa:string;
+  nombreTrabajador:string;
+  apellidoTrabajador:string;
   tipoTrabajador:string;
+  passTrabajador:string;
+  pass2Trabajador:string;
   idBusqueda:number;
-  ngOnInit() {
-  }
+  errRut:string;
+  //errPass:string;
 
-  registrar(){
-    console.log("tipo usuario:"+ this.tipoTrabajador);
-    this.trabajador.tipoTrabajador=this.tipoTrabajador;
-    console.log("tipo usuario "+this.trabajador.tipoTrabajador);
-    if(this.validarPass){
-      this.trabajador.empresa=this.empresa.nombreEmpresa;
-      this.empresaService.crearEmpresa(this.empresa).subscribe(params=>{
-        this.empresa=params;
-      });
-      this.trabajadorServicio.crearTrabajador(this.trabajador).subscribe(data=>{
-        this.trabajador=data;
-        console.log(data);
-      });
-      this.router.navigate(['empresa/login']);
+  formRegistroEmpresa: FormGroup;
+
+  regexp: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3,4})+$/;
+
+  constructor(private router:Router, private empresaService:EmpresaServiceService,private trabajadorServicio:TrabajadorServiceService, private formBuilder:FormBuilder) { }
+
+
+  ngOnInit() {
+    this.formRegistroEmpresa = this.formBuilder.group({
+  
+    RutEmpresa:['', Validators.required],
+    NombreEmpresa:['', Validators.required],
+    EmailEmpresa:['', Validators.required],
+    NombreTrabajador:['', Validators.required],
+    ApellidoTrabajador:['', Validators.required],
+    TipoTrabajador:['', Validators.required],
+    PassTrabajador:['', Validators.required],
+    Pass2Trabajador:['', Validators.required]});
     }
-  } 
+
+    registrar(){
+      this.empresa.rutEmpresa=this.formatRut(this.rutEmpresa);
+      this.empresa.nombreEmpresa=this.nombreEmpresa;
+      this.empresa.emailEmpresa=this.emailEmpresa;
+      this.trabajador.nombreTrabajador=this.nombreTrabajador;
+      this.trabajador.apellidoTrabajador=this.apellidoTrabajador;
+      this.trabajador.tipoTrabajador=this.tipoTrabajador;
+      this.trabajador.passTrabajador=this.passTrabajador;
+      this.trabajador.empresa=this.empresa.nombreEmpresa;
+      this.empresaService.crearEmpresa(this.empresa).subscribe(params=>{})
+      this.trabajadorServicio.crearTrabajador(this.trabajador).subscribe(data=>{})
+      this.router.navigate(['empresa/login']);
+    } 
+
+    validateRut(){
+  
+      let validar:ValidarRut  = new ValidarRut();
+      let resultado = validar.esValido( this.rutEmpresa);
+      if(resultado.result ){
+        this.errRut="";
+      }else{
+        this.errRut=resultado.message;
+      }
+    }
+
+    formatRut(rut:string):number{
+      let rutNumeros;
+      if(rut.length>9){
+        rutNumeros= rut.substr(0,8); 
+     }else{
+      rutNumeros= rut.substr(0,7); 
+     }
+      console.log (rutNumeros)
+      return Number(rutNumeros);
+    }
+
+  /*  validarPass(){
+      let validar:ValidarContrasena  = new ValidarContrasena();
+      let resultado = validar.esValido( this.passTrabajador,this.pass2Trabajador);
+      if(resultado.result ){
+        this.errPass="";
+      }else{
+        this.errPass=resultado.message;
+      }
+    } */
 
   loginEmpresa(){
   	this.router.navigate(['empresa/login']);
@@ -45,18 +102,10 @@ export class RegistroEmpresaComponent implements OnInit {
   homeEmpresa(){
   	this.router.navigate(['home_empresa']);
   }
-  validarPass(){
-    let iguales:boolean=false;
-    if(this.pass2=this.trabajador.passTrabajador){
-      iguales=true;
-    }
-    return iguales; 
-  }
+
   RegistrarEmpresa(){
-    
     this.trabajadorServicio.crearTrabajador(this.trabajador).subscribe();
     this.empresaService.crearEmpresa(this.empresa);
-
   }
   //buscarPorId(): vacio -> vacio
   //guarda el id de busqueda idbusqueda y 
@@ -64,7 +113,6 @@ export class RegistroEmpresaComponent implements OnInit {
   buscarPorId(){
     localStorage.setItem("idBusqueda",""+this.idBusqueda);
     this.router.navigate(['buscar_id']);
-    
   }
   home(){
     this.router.navigate(['home']);
