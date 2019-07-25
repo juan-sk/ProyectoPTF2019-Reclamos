179094afcbd49135b418a09ef1e652e97c7300c3
juan <juan.ramirez.sk@gmail.com>
2019-07-25 13:17:40 -0400

    fix vista embebida

--- FrontAngular/v1/src/app/componentes/realizar-reclamo-embebida/realizar-reclamo-embebida.component.ts
+++ FrontAngular/v1/src/app/componentes/realizar-reclamo-embebida/realizar-reclamo-embebida.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ReclamoSugerencia } from 'src/app/Modelo/ReclamoSugerencia';
 import {RsServiceService} from 'src/app/Services/rs-service.service';
 import { Router, ActivatedRoute } from '@angular/router';
+import { EnviarEmailService } from 'src/app/Services/enviar-email.service';
 
 @Component({
   selector: 'app-realizar-reclamo-embebida',
@@ -16,7 +17,7 @@ export class RealizarReclamoEmbebidaComponent implements OnInit {
   empresa:string;
   sesion:boolean=false;
   idEmpresa;
-  constructor(private router:Router,private serviceRS:RsServiceService,private pathVariavles:ActivatedRoute) { }
+  constructor(private servicioMail:EnviarEmailService,private router:Router,private serviceRS:RsServiceService,private pathVariavles:ActivatedRoute) { }
 
   ngOnInit() {
     if (localStorage.getItem('Email')!="anonimo"){
@@ -27,24 +28,67 @@ export class RealizarReclamoEmbebidaComponent implements OnInit {
   console.log("id_ "+this.idEmpresa);
   }
   realizarReclamoSugerencia() {
-    this.rs.idEmpresa=this.idEmpresa;
-    this.rs.idReclamoSugerencia=0;
-    this.rs.idEmpleado=0;
-    this.rs.estado="en proseso";
-    this.rs.tipo=this.tipo
-    this.rs.respuestaRS="aun no tiene respuestas";
-    
-    this.rs.fechaReclamoSugerencia=new Date();
+    try {
+      
+      this.rs.idEmpresa=this.idEmpresa;
+      console.log(this.rs.idEmpresa);
+      this.rs.tipo=this.tipo;
+        
+      this.rs.idReclamoSugerencia=0;
+      this.rs.idEmpleado=0;
+      this.rs.estado="en proceso";
+      
+      this.rs.respuestaRS="aun no tiene respuesta";
+      
+      this.rs.fechaReclamoSugerencia=new Date();
+      
+      localStorage.setItem("tituloRS",this.rs.tituloRS);
+      localStorage.setItem("empresa",this.rs.idEmpresa+"");
+      localStorage.setItem("idRS",""+this.rs.idReclamoSugerencia);
+      localStorage.setItem("fecha",this.rs.fechaReclamoSugerencia.toString());
+      localStorage.setItem("detalleRS",this.rs.detalleReclamoSugerencia);
+      localStorage.setItem("tipo",this.rs.tipo);
+        
+      this.rs.fechaResuelto=new Date("2019-01-01");
+      console.log("nombre empresa: "+this.rs.idEmpresa);
+      console.log(this.rs.fechaResuelto.toDateString());
+      this.serviceRS.crearReclamo(this.rs).subscribe(data =>{this.rs= data});
 
-    localStorage.setItem("tituloRS",this.rs.tituloRS);
-    localStorage.setItem("empresa",""+this.rs.idEmpresa);
-    localStorage.setItem("idRS",""+this.rs.idReclamoSugerencia);
-    localStorage.setItem("fecha",this.rs.fechaReclamoSugerencia.toString())
-    localStorage.setItem("detalleRS",this.rs.detalleReclamoSugerencia);
-    localStorage.setItem("tipo",this.rs.tipo);
-    this.serviceRS.crearReclamo(this.rs).subscribe(data =>{this.rs= data});
-    alert("reclamo generado enviado con exito ");
-    this.router.navigate(["perfil"]);
+      try {
+        this.serviceRS.getLastReclamoUsuario(this.rs.usuarioReclamoSugerencia).subscribe(data=>{
+          let rs:ReclamoSugerencia=data;
+            
+          localStorage.setItem("idRS",""+rs.idReclamoSugerencia);
+          console.log(localStorage.getItem("idRS"));
+          //alert("reclamo generado enviado con exito ");
+          //this.enviarEmail();
+          this.servicioMail.sendEmail(+localStorage.getItem("idUsuario"));
+          console.log("hola");
+          this.router.navigate(["rs_enviado"]);
+        });
+      } catch (error) {
+        this.serviceRS.getLastReclamo().subscribe(data=>{
+          let rs:ReclamoSugerencia=data;
+            
+          localStorage.setItem("idRS",""+rs.idReclamoSugerencia);
+          console.log(localStorage.getItem("idRS"));
+          //alert("reclamo generado enviado con exito ");
+          this.router.navigate(["rs_enviado"]);
+        });
+      }finally{
+        this.serviceRS.getLastReclamo().subscribe(data=>{
+          let rs:ReclamoSugerencia=data;
+            
+          localStorage.setItem("idRS",""+rs.idReclamoSugerencia);
+          console.log(localStorage.getItem("idRS"));
+          //alert("reclamo generado enviado con exito ");
+          this.router.navigate(["rs_enviado"]);
+      });
+      }
+     
+    } catch (error) {
+    console.log("error");
+    }
   }
   login(){
     this.router.navigate(['login']);
