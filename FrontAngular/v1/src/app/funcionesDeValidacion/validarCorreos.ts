476dac451a0e3c8ae91ddc3be973513a7c63435e
joseCris <tecladoql@gmail.com>
2019-07-25 13:28:17 -0400

    documentacion validaciones y registro usuario

--- FrontAngular/v1/src/app/funcionesDeValidacion/validarCorreos.ts
+++ FrontAngular/v1/src/app/funcionesDeValidacion/validarCorreos.ts
@@ -3,6 +3,9 @@ export class ValidarCorreos{
     constructor(){
       
     }
+    //metodo esValido recive dentro 2 parametros de tipo string y los compara 
+    //de esta manera valida si ambos correos son iguales
+    //el mensaje correo OK a pesar que es un retorno no llega a mostrarse en pantalla
     esValido(correo,correo2) {
         if (correo == correo2){
             return {result: true, message:"* correo OK"}; 
