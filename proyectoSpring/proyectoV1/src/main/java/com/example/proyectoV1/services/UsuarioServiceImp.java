fdbd3b2a303c625eaf2ac8f4150734c6f700a433
suZanahoria287 <zerimar287@gmail.com>
2019-08-02 18:08:33 -0400

    Documentacion UsuarioService y UsuarioServiceImp

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/services/UsuarioServiceImp.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/services/UsuarioServiceImp.java
@@ -10,36 +10,44 @@ public class UsuarioServiceImp  implements UsuarioService{
 	@Autowired
 	private UsuarioRepositorio repositorio;
 	////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Agregar
-	@Override
+	//add: Usuario -> Usuario
+	//recibe un usuario y lo almacena en la DB, devuelve Usuario
+	//Ej: add(Usuario x) devuelve Usuario	@Override
 	public Usuario add(Usuario p) {
 		return repositorio.save(p);
 	}
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Editar
-	@Override
+	//edit: Usuario -> Usuario
+	//recibe un Usuario y lo edita en la DB, devuelve Usuario
+	//Ej: edit(Usuario) devuelve Usuario	@Override
 	public Usuario edit(Usuario p) {
 		return repositorio.save(p);
 	}
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Eliminar
-	@Override
+	//delete: Usuario -> void
+	//recibe un usuario y lo elimina en la DB
+	//Ej: delete(Usuario x)	@Override
 	public void delete(Usuario p) {
 		repositorio.delete(p);
 	}
     ////////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Lista de Todos los Usuarios
-	@Override
+	//listar: void -> List<Usuario>
+	//devuelve una lista de todos los usuarios de la DB
+	//Ej: listar() devuelve List<Usuario>	@Override
 	public List<Usuario> listar() {
 		return repositorio.findAll();	
 	}
-	//Buscar un Usuario por su Rut
+	//listarId_RutUsuario: int -> Usuario
+	//recibe un id de usuario de tipo int, busca el usuario y lo retorna
+	//Ej: listarId_RutUsuario(int id) devuelve Usuario
 	public Usuario listarId_RutUsuario(int id_rutUsuario) {
 		return repositorio.findOne(id_rutUsuario);
 	}
 	///////////////////////////////////////////////////////////////////////////////////////////////////////
-	//LogIn Usuario
-	@Override
+	//LogIn: Usuario -> Usuario
+	//revibe un Usuario, lo compara con la DB, si coincide Devuelve un Usuario, si no,
+	//lanza una excepcion.
+	//Ej: logIn(Usuario x) devuelve Usuario	@Override
 	public Usuario logIn(Usuario p) throws LoginException{
 		Usuario usuarioAVerificar=p;
 		String emailusuario = p.getEmailUsuario();
