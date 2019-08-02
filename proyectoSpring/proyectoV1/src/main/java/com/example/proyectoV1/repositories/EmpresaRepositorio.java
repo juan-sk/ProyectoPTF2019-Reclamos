a25aae1f5251a6951fcd5f9af0000c0dc6127948
suZanahoria287 <zerimar287@gmail.com>
2019-08-02 15:10:40 -0400

    Documentacion EmpresaRepositorio

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/EmpresaRepositorio.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/EmpresaRepositorio.java
@@ -6,19 +6,29 @@ import com.example.proyectoV1.entities.Empresa;
 
 public interface EmpresaRepositorio extends Repository<Empresa,Integer>{
 	//////////////////////////////////////////////////////////////////////////////////////////////////
-	//Agregar
+	//save: Empresa -> Empresa
+	//recibe una Empresa y la guarda en la DB, retorna la Empresa
+	//Ej: save(Empresa x) devuelve Empresa x
 	Empresa save(Empresa e);
 	/////////////////////////////////////////////////////////////////////////////////////////////////
-	//Eliminar
+	//delete: int -> void
+	//recibe un rut de empresa de tipo int, y la elimina. 
+	//Ej: delete(int rut) 
 	void delete(int rutEmpresa);
 	/////////////////////////////////////////////////////////////////////////////////////////////////
-	//Encontrar Empresa por rut
+	//findBy: int -> Empresa
+	//Busca una empresa por su rut, devuelve esa empresa
+	//Ej: findOne(int rut) devuelve Empresa 
 	Empresa findOne(int rutEmpresa);
 	////////////////////////////////////////////////////////////////////////////////////////////////
-	//Listar todas las Empresas
+	//findAll: void -> List<Empresa>
+	//devuelve una lista de todas las Empresas de la DB
+	//Ej: findAll() devuelve List<Empresa>
 	List<Empresa> findAll();
 	////////////////////////////////////////////////////////////////////////////////////////////////
-	//Encontrar empresa por nombre
+	//findBynombeEmpresa: String -> Empresa
+	//Busca una empresa por su nombre y la retorna
+	//Ej: findBynombreEmpresa(String nombre) devuelve Empresa
 	Empresa findBynombreEmpresa(String nombreEmpresa);
 	
 }
