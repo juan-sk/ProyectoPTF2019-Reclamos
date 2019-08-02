81fe0d4cb0dc294199dcaa5d1d9a7dc0ade39dae
juan <juan.ramirez.sk@gmail.com>
2019-08-02 17:42:48 -0400

    documentacion app component

--- FrontAngular/v1/src/app/app.component.ts
+++ FrontAngular/v1/src/app/app.component.ts
@@ -12,22 +12,32 @@ import { Router } from '@angular/router';
 })
 export class AppComponent implements OnInit{
   constructor(private router:Router){}
+  title = 'v1';
+  //este metodo se ejecuta al momento de iniciar el componente
   ngOnInit() {
    
   }
+
+  //home(): vacio->vacio
+  //este metodo redirecciona al componente home
   home(){
     this.router.navigate(['home']);
   }
+  //logIn(): vacio->vacio 
+  // este metodo redirecciona al componente login
   login(){
     this.router.navigate(['login']);
   }
+  //regustrar():vacio->vacio
+  //este metodo redirecciona al componente registrar
   registrar(){
     this.router.navigate(['registrar']);
   }
-
+  //nuestroEquipo():vacio->vacio 
+  // este metodo redirecciona al componente nuestro_equipo
   nuestroEquipo(){
     this.router.navigate(['nuestro_equipo']);
   }
 
-  title = 'v1';
+
 }
