d2aefb4f2ab7363e0ad0be4f11f62f0fc9d77c4c
juan <juan.ramirez.sk@gmail.com>
2019-08-02 13:36:26 -0400

    documentacion trabajadores

--- FrontAngular/v1/src/app/componentes/empresa/trabajadores/trabajadores.component.ts
+++ FrontAngular/v1/src/app/componentes/empresa/trabajadores/trabajadores.component.ts
@@ -11,12 +11,14 @@ import { Empresa } from 'src/app/Modelo/Empresa';
   styleUrls: ['./trabajadores.component.css']
 })
 export class TrabajadoresComponent implements OnInit {
-
-  constructor(private router:Router,private servicioEmrpesa:EmpresaServiceService,private servicioTrabajador:TrabajadorServiceService) { }
+  //atributos
   infoTrabajador:Trabajador=JSON.parse(localStorage.getItem("trabajador"));
   infoEmrpesa:Empresa=JSON.parse(localStorage.getItem("empresa"));
   trabajadores:Trabajador[]=[];
   administrador:boolean=false;
+
+  constructor(private router:Router,private servicioEmrpesa:EmpresaServiceService,private servicioTrabajador:TrabajadorServiceService) { }
+  //este metodo se ejecuta al momento de iniciar el componente 
   ngOnInit() {
     if(this.infoTrabajador==null){
       this.router.navigate(["home"]);
@@ -30,21 +32,31 @@ export class TrabajadoresComponent implements OnInit {
    
 
   }
+  //eliminarTrabajador(Trabajador): Trabajador-> vacio
+  // ejecuta el metodo eliminarTrabajador del servicio TrabajadorService
   eliminarTrabajador(trabajador:Trabajador){
     this.servicioTrabajador.eliminarTrabajador(trabajador.idTrabajador).subscribe(data=>{location.reload();});
 
   }
+  //agregarTrabajador():
+  //redirecciona al componente agregar trabajador de empresa
   agregarTrabajador(){
     this.router.navigate(["empresa/agregarTrabajador"]);
   }
+  //irTrabajadores():vacio->vacio
+  //redirige al componente trabajadores de empresa
   irTrabajadores(){
     this.router.navigate(["empresa/listarTrabajadores"]);
   }
   
+  //irSugerencia(): vavio->vacio (redireccion a componente listar sugerencias)
+  //al llamar el metodo se realida el redireccionamiento al componente listar sugerencia
+  //a travez del path empreza/listarSugerencias, este componente es parte de la vista de trabajador de la empresa
   irSugerencia(){
     this.router.navigate(["empresa/listaSugerencias"]);
   }
-  
+  //verEstadisticas():vacio->vacio
+  //redirecciona al componente estadisticas de empresa
   verEstadisticas(){
     this.router.navigate(["empresa/estadisticas"]);
   }
@@ -60,9 +72,14 @@ export class TrabajadoresComponent implements OnInit {
   irReclamo(){
     this.router.navigate(["empresa/listaReclamos"]);
   }
+
+  //homeEmpresa():vacio->vacio
+  //redirecciona al componente home_empresa
   homeEmpresa(){
     this.router.navigate(['home_empresa']);
   }
+  //cerrarSesion():vavio-> vacio
+  //al ejecutar el metodo se borra la informacion de el localstorage y rediririge al componente home_empresa
   cerrarSesion(){
     localStorage.clear();
     this.router.navigate(['home_empresa']);
