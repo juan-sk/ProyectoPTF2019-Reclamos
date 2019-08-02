4976857fefa8dfdfda28a382f906f122aebc433e
suZanahoria287 <zerimar287@gmail.com>
2019-08-02 17:22:51 -0400

    Documentacion UsuarioRepositorio

--- proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/UsuarioRepositorio.java
+++ proyectoSpring/proyectoV1/src/main/java/com/example/proyectoV1/repositories/UsuarioRepositorio.java
@@ -4,22 +4,34 @@ import com.example.proyectoV1.entities.Usuario;
 import java.util.List;
 public interface UsuarioRepositorio  extends Repository<Usuario,Integer>{	
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Create
+	//save: Usuario -> Usuario
+	//recibe un Usuario y lo guarda en la DB
+	//Ej: save(Usuario x) devuelve Usuario
 	Usuario save(Usuario p);
 	/////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Eliminar
+	//delete: Usuario -> void
+	//recibe un Usuario y lo elimina
+	//Ej: delete(Usuario x)
 	void delete(Usuario p);
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Lista de todos los Usuarios
+	//findAll: void -> List<Usuario>
+	//devuelve una List de todos los usuarios
+	//Ej: findAll() devuelve List<Usuario>
 	List<Usuario>findAll();
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Encontrar un Usuario por su Rut
+	//findOne: int -> Usuario
+	//recibe un rut de usuario, lo encuentra y lo devuelve
+	//findOne(int rut) devuelve Usuario
 	Usuario findOne(int id_rutUsuario);
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Encontrar un Usuario por su Email
+	//findByEmailUsuario: String -> Usuario
+	//recibe un email de usuario, lo encuentra y lo devuelve
+	//Ej: findByEmailUsuario(String email) devuelve Usuario
 	Usuario findByEmailUsuario(String emailUsuario);
 	////////////////////////////////////////////////////////////////////////////////////////////////////
-	//Encontrar un Usuario por su Email y Contraseña
+	//findByEmailUsuarioAndPassUsuario: String, String -> Usuario
+	//encuentra un usuario por su email y su contraseña, devuelve usuario
+	//Ej:findByEmailUsuarioAndPassUsuario(String email, String password) devuelve Usuario
 	Usuario findByEmailUsuarioAndPassUsuario(String emailUsuario, String passUsuario);  
 }   
   
