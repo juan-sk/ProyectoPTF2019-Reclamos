8cac839c49a524c7fec5da5e352bcbbb5f1e7c7f
juan <juan.ramirez.sk@gmail.com>
2019-08-02 15:03:25 -0400

    documentacion mi perfil

--- FrontAngular/v1/src/app/componentes/mi-perfil/mi-perfil.component.ts
+++ FrontAngular/v1/src/app/componentes/mi-perfil/mi-perfil.component.ts
@@ -32,26 +32,37 @@ export class MiPerfilComponent implements OnInit {
     this.rutUsuario=localStorage.getItem("idUsuario");
 
   } 
+
   realizarReclamo() {
   	this.router.navigate(['realizar_reclamo']);
   }
+  //realizarSugerencia: void -> void
+  //Redirige a la vista realizar_sugerencia
   realizarSugerencia(){
     this.router.navigate(['realizar_sugerencia']);
   }
 
+  //cerrarSesion():vavio-> vacio
+  //al ejecutar el metodo se borra la informacion de el localstorage y rediririge al componente home_empresa
   cerrarSesion(){
     localStorage.setItem("Email", "anonimo");
     this.router.navigate(["home"])
   }
 
+  //buscarPorId(): vacio -> vacio
+  //guarda el id de busqueda idbusqueda y 
+  //redirige al componente buscar_id
   buscarPorId(){
     localStorage.setItem("idBusqueda",""+this.idBusqueda);
     this.router.navigate(['buscar_id']);
     
   }
+  //home: void -> void
+  //Redirige a la vista Home
   home(){
     this.router.navigate(['home']);
   }
+  //redirige al componente sugerencia
   irNosotros(){
     this.router.navigate(["nuestro_equipo"]);
   }
